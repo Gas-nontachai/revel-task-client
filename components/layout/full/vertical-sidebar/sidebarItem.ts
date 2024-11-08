9bc80352e739c8b37aefcfa994ab2c809b4758e4
Dui Jirawat <33535186+Fatherfish@users.noreply.github.com>
2024-11-08 09:51:10 +0700

    Update sidebarItem.ts

--- components/layout/full/vertical-sidebar/sidebarItem.ts
+++ components/layout/full/vertical-sidebar/sidebarItem.ts
@@ -32,93 +32,22 @@ const sidebarItem: { header: string, childs: Menu[] }[] = [
     header: 'รายการรับสินค้า',
     childs: [
       {
-        key: 'invoice-supplier',
-        title: 'ตามบริษัท',
+        key: 'my-task',
+        title: 'My Task',
         icon: ReportAnalyticsIcon,
-        to: '/invoice-supplier'
+        to: '/my-task'
       },
       {
-        key: 'invoice-supplier',
-        title: 'ตามสาขา',
+        key: 'project',
+        title: 'Project Manage',
         icon: ReportAnalyticsIcon,
-        to: '/invoice-supplier-branch'
+        to: '/project'
       },
-      {
-        key: 'invoice-supplier',
-        title: 'ตามบัญชีการโอน',
-        icon: ReportAnalyticsIcon,
-        to: '/invoice-supplier-book-bank'
-      }
     ]
-  },
-  {
-    header: 'รายงาน',
-    childs: [
-      {
-        key: 'report-product-invoice',
-        title: 'รับสินค้าตามสินค้า',
-        icon: ReportAnalyticsIcon,
-        to: '/report-product-invoice'
-      },
-      {
-        key: 'report-supplier-invoice',
-        title: 'รับสินค้าตาม Supplier',
-        icon: ReportAnalyticsIcon,
-        to: '/report-supplier-invoice'
-      },
-      {
-        key: 'report-payment',
-        title: 'การโอนเงิน',
-        icon: ReportAnalyticsIcon,
-        to: '/report-payment'
-      }
-    ],
   },
   {
     header: 'ข้อมูลหลัก',
     childs: [
-      {
-        key: 'supplier',
-        title: 'ข้อมูล Supplier',
-        icon: UsersIcon,
-        to: '/supplier'
-      },
-      {
-        key: 'product',
-        title: 'ข้อมูลสินค้า',
-        icon: FolderIcon,
-        to: '/product'
-      },
-      {
-        key: 'product-type',
-        title: 'ประเภทสินค้า',
-        icon: Category2Icon,
-        to: '/product-type'
-      },
-      {
-        key: 'product-category',
-        title: 'หมวดหมู่สินค้า',
-        icon: CategoryIcon,
-        to: '/product-category'
-      },
-      {
-        key: 'company',
-        title: 'ข้อมูลบริษัท',
-        icon: SmartHomeIcon,
-        to: '/company'
-      },
-      {
-        key: 'branch',
-        title: 'ข้อมูลสาขา',
-        icon: MapPinsIcon,
-        to: '/branch'
-      },
-      {
-        key: 'book-bank',
-        title: 'สมุดบัญชี',
-        icon: FileIcon,
-        to: '/book-bank'
-      },
       {
         key: 'user',
         title: 'ผู้ใช้งานระบบ',
